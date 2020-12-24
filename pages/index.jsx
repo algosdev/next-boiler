b10e58d02f723b4a8b00fb94b78fccef970eef90
Oripovd1 <oripovd1@gmail.com>
2020-12-24 15:23:37 +0500

    change products, posters

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,33 +4,76 @@ import SEO from '../components/seo'
 import BannerContainer from '../components/bannerContainer/bannerContainer'
 import Banner from '../components/banner/banner'
 
+const appleProducts = {
+  title: 'Apple products',
+  catalog: [
+    {
+      name: 'iPhone',
+      image: 'images/iphone.png',
+    },
+    {
+      name: 'iPad',
+      image: 'images/ipad.png',
+    },
+    {
+      name: 'Apple Watch',
+      image: 'images/apple_watch.png',
+    },
+    {
+      name: 'AirPods',
+      image: 'images/airpods.png',
+    },
+    {
+      name: 'Macbook',
+      image: 'images/mac_m1.jpg',
+    },
+    {
+      name: 'iMac',
+      image: 'images/imac.jpg',
+    },
+    {
+      name: 'Accessories',
+      image: 'images/accessories.png',
+    },
+  ],
+}
+
+const samsungProducts = {
+  title: 'Samsung products',
+  catalog: [
+    {
+      name: 'Galaxy smartphones',
+      image: 'images/galaxy_s20.jpg',
+    },
+    {
+      name: 'Tablets',
+      image: 'images/samsung_tabs.jpg',
+    },
+    {
+      name: 'Watches',
+      image: 'images/samsung_watches.png',
+    },
+    {
+      name: 'Buds',
+      image: 'images/galaxy_buds.png',
+    },
+  ],
+}
+
 export default function Home() {
   return (
     <>
       <SEO />
       <Hero />
-      <CatalogList title='Apple products' />
-      <BannerContainer>
-        <Banner
-          size={9}
-          name='banner-po-seredine-1300x260'
-          src='images/cash_back.jpg'
-        />
-        <Banner
-          size={3}
-          name='malyi-nizhnii-banner-420x260'
-          src='images/delivery.jpg'
-        />
-      </BannerContainer>
-      <CatalogList title='Samsung products' />
+      <CatalogList data={appleProducts} />
       <BannerContainer>
         <Banner
           size={12}
           name='pervyi-banner-1720x260'
-          src='images/payment_plan.jpg'
+          src='images/poster_watches.jpg'
         />
       </BannerContainer>
-      <CatalogList title='Acoustics' />
+      <CatalogList data={samsungProducts} />
     </>
   )
 }
