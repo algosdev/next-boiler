977bfe490563cda511c970f9c395d3cb10ca638e
algo <samandar149official@gmail.com>
2021-01-11 18:16:20 +0500

    Bugs fixed

--- components/compare/compareFilter.jsx
+++ components/compare/compareFilter.jsx
@@ -1,33 +1,21 @@
 import React from 'react'
-import compare from '../../pages/compare'
 import style from './compare.module.scss'
 import CompareContent from './compareContent'
+import CompareOption from './compareOption'
+
+const compareOptionData = [
+  ['MacBook', 'MacBook Air', 'MacBook Pro'],
+  ['MacBook', 'MacBook Air', 'MacBook Pro'],
+  ['MacBook', 'MacBook Air', 'MacBook Pro'],
+]
 function CompareFilter() {
   return (
     <div className={style.wrapper_filter}>
       <div className={style.main_title}>Сравните модели Mac</div>
       <div className={style.grid_option}>
-        <div className={style.option}>
-          <select>
-            <option>MacBook</option>
-            <option>MacBook Air</option>
-            <option>MacBook Pro</option>
-          </select>
-        </div>
-        <div className={style.option}>
-          <select>
-            <option>MacBook</option>
-            <option>MacBook Air</option>
-            <option>MacBook Pro</option>
-          </select>
-        </div>
-        <div className={style.option}>
-          <select>
-            <option>MacBook</option>
-            <option>MacBook Air</option>
-            <option>MacBook Pro</option>
-          </select>
-        </div>
+        {compareOptionData.map((item, index) => (
+          <CompareOption data={item} key={index} />
+        ))}
       </div>
       <CompareContent />
     </div>
