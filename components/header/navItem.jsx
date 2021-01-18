28a0f10cc6377036b6f5ebd90833b38fbaaa291f
Samandar <samandar149official@gmail.com>
2021-01-18 20:46:28 +0500

    6

--- components/header/navItem.jsx
+++ components/header/navItem.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react';
 import {
   ClickAwayListener,
   Grow,
@@ -7,10 +7,10 @@ import {
   MenuItem,
   MenuList,
   makeStyles,
-} from '@material-ui/core'
-import style from './header.module.scss'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import { Link } from '../../i18n'
+} from '@material-ui/core';
+import style from './header.module.scss';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Link } from '../../i18n';
 const useStyles = makeStyles(() => ({
   paper: {
     borderRadius: '6px',
@@ -23,7 +23,7 @@ const useStyles = makeStyles(() => ({
     },
     '& .MuiMenuItem-root a': {
       color: '#fff',
-      margin: '6px 20px',
+      padding: '6px 20px',
       minWidth: '100px',
       justifyContent: 'flex-start !important',
     },
@@ -42,39 +42,39 @@ const useStyles = makeStyles(() => ({
     paddingTop: '10px',
     top: '0 !important',
   },
-}))
+}));
 const NavItem = ({ title, subCategs }) => {
-  NavItem.muiName = Popper.muiName
-  const classes = useStyles()
-  const [open, setOpen] = useState(false)
-  const anchorRef = useRef(null)
+  NavItem.muiName = Popper.muiName;
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
   const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen)
-  }
+    setOpen((prevOpen) => !prevOpen);
+  };
 
   const handleClose = (event) => {
     if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return
+      return;
     }
 
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
   function handleListKeyDown(event) {
     if (event.key === 'Tab') {
-      event.preventDefault()
-      setOpen(false)
+      event.preventDefault();
+      setOpen(false);
     }
   }
 
-  const prevOpen = useRef(open)
+  const prevOpen = useRef(open);
   useEffect(() => {
     if (prevOpen.current === true && open === false) {
-      anchorRef.current.focus()
+      anchorRef.current.focus();
     }
 
-    prevOpen.current = open
-  }, [open])
+    prevOpen.current = open;
+  }, [open]);
 
   return (
     <li
@@ -130,7 +130,7 @@ const NavItem = ({ title, subCategs }) => {
         )}
       </Popper>
     </li>
-  )
-}
+  );
+};
 
-export default NavItem
+export default NavItem;
