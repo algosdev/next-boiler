4fe9a71bc8d67fbb7b956dc386189cafda4be059
Samandar <samandar149official@gmail.com>
2021-02-04 18:17:24 +0500

    Login btn in header

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,14 +1,17 @@
 import Header from './header/header';
 import Footer from './footer/footer';
-
+import { useDispatch, shallowEqual, useSelector } from 'react-redux';
 export default function Layout({ children, categories }) {
   const filteredCategories = categories?.categories?.sort(
     (a, b) => parseInt(a.order) - parseInt(b.order)
   );
-
+  const user = useSelector((state) => state.auth.user, shallowEqual);
   return (
     <div className='main_container' id='macbro_container'>
-      <Header categories={filteredCategories} />
+      <Header
+        categories={filteredCategories}
+        hasLogged={user?.id ? true : false}
+      />
       <div className='header_margin'></div>
       {children}
       <Footer />
